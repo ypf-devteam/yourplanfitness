9883feac944cffb108ae8a0fab3382206b4c3156
ypf-devteam <devteam@yourplanfitness.com>
2021-01-17 18:39:47 -0600

    stages changes to Login.dart

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,5 +1,6 @@
 import 'package:your_plan_fitness/Animation/FadeAnimation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:your_plan_fitness/NavController.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
